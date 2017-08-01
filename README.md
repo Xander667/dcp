72c2b4acd8ff5ebf8086a739f9a0fbea9dd05fae
Xander Harris <xanderh@hotmail.co.uk>
2017-07-31 20:35:29 -0700

    ci testing

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dcp
 React web app to host code projects
+
+npm run build   <--- build code >
+
+serve -s build  <--- run server >
+ http://localhost:5000 
--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+          <h3>Continuous Integration</h3>
+          <h4>WORKING!</h4>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
